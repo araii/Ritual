daa9829e301c7d3e85c1600c8151e0875fdb3efd
Arai <pearcearai@gmail.com>
2018-04-07 10:28:51 -0400

    add delete

--- Ritual/ViewControllers/TaskTableViewController.swift
+++ Ritual/ViewControllers/TaskTableViewController.swift
@@ -54,7 +54,20 @@ extension TaskTableViewController {
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
-  
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath:
+        IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle:
+        UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return tasks.count
   }
